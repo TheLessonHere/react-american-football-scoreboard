bf83b4135ad8aacb586348a8e14346e78846972f
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-07-15 15:13:58 -0600

    Click functionality added for Down, ToGo, and Quarter

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -3,23 +3,42 @@ import "./App.css";
 
 const BottomRow = () => {
 
+  const [currentdown, setCurrentDown] = useState(1)
+  const [togo, setToGo] = useState(10)
+  const [quarter, setQuarter] = useState(1)
+  
+  const handlerDownCalc = (value) => {
+    if (value < 4) {return value + 1}
+    else {return 1}
+  }
+
+  const handlerToGoCalc = (value) => {
+    if (value > 1) {return value - 1}
+    else {return 10}
+  }
+
+  const handlerQuarterCalc = (value) => {
+    if (value < 4) {return value + 1}
+    else {return 1}
+  }
+
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value clickable" onClick={() => setCurrentDown(handlerDownCalc(currentdown))}>{currentdown}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value clickable" onClick={() => setToGo(handlerToGoCalc(togo))}>{togo}</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">50</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value clickable" onClick={() => setQuarter(handlerQuarterCalc(quarter))}>{quarter}</div>
       </div>
     </div>
   );
