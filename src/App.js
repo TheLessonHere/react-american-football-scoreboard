243b99b2c4fb2d7733b818fb466b57ed1966a4ad
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-07-15 17:06:34 -0600

    Some functionality added for the timer, not quite finished}

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -8,6 +8,37 @@ function App() {
 
   const [homescore, setHomeScore] = useState(0);
   const [awayscore, setAwayScore] = useState(0);
+  const [timeleft, setTimeLeft] = useState(900);
+
+  const convertedTimer = (seconds) => {
+    let minutesleft = Math.floor(seconds / 60);
+    let secondsleft = seconds % 60;
+    if (secondsleft < 10) {
+      return `${minutesleft}:0${secondsleft}`
+    } else {
+    return `${minutesleft}:${secondsleft}`
+    }
+  }
+
+  var runningTimer;
+
+  // const startTimer = () => {
+  //   setTimeLeft(timeleft-1);
+  //   useEffect(() => {
+  //     function timerIncrement(value) {
+  //       if (value > 0) {
+  //         setTimeLeft(value - 1)
+  //       } else {
+  //         clearInterval(runningTimer);
+  //         setTimeLeft(0)
+  //       }
+  //     }
+  //     runningTimer = setInterval(timerIncrement(timeleft), 1000);
+  //   });}
+
+  // const stopTimer = () => {
+  //   clearInterval(runningTimer);
+  // }
 
   return (
     <div className="container">
@@ -20,7 +51,9 @@ function App() {
 
             <div className="home__score">{homescore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">
+            {convertedTimer(timeleft)}
+          </div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayscore}</div>
@@ -31,10 +64,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          {/* <button className="start__timer" onClick={() => startTimer()}>Start Timer</button> */}
           <button className="homeButtons__touchdown" onClick={() => setHomeScore(homescore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homescore + 3)}>Home Field Goal</button>
           </div>
         <div className="awayButtons">
+          {/* <button className="stop__timer" onClick={() => stopTimer()}>Stop Timer</button> */}
           <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayscore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayscore + 3)}>Away Field Goal</button>
         </div>
@@ -42,5 +77,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
